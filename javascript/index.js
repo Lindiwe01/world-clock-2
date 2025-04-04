127e5677a9ab7fe164904810b0b0aa1e045b1f09
Lindiwe01 <motaung.lindiwe01@gmail.com>
2025-04-04 14:30:00 +0200

    Added functionality for this select

--- javascript/index.js
+++ javascript/index.js
@@ -1,38 +1,48 @@
-setInterval(function (updateTime) {
+function updateTime() {
   //johannesburg
   let johannesburgElement = document.querySelector("#johannesburg");
-  let johannesburgDateElement = johannesburgElement.querySelector(".date");
-  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-  johannesburgTime = moment().tz("Africa/Johannesburg");
-  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-  johannesburgTimeElement.innerHTML = johannesburgTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    johannesburgTime = moment().tz("Africa/Johannesburg");
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   //nairobi
   let nairobiElement = document.querySelector("#nairobi");
-  let nairobiDateElement = nairobiElement.querySelector(".date");
-  let nairobiTimeElement = nairobiElement.querySelector(".time");
-  nairobiTime = moment().tz("Africa/nairobi");
-  nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do YYYY");
-  nairobiTimeElement.innerHTML = nairobiTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}, 1000);
-
-function updatecity(event) {
-  let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = "
-  <div class="city">
-  <div>
-    <h2>+[cityName]</h2>
-    </div>
-  </div>";
+  if (nairobiElement) {
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    nairobiTime = moment().tz("Africa/nairobi");
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
-let citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updatecity);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
 
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
